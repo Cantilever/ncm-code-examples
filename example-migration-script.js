cf36eb5ac015f5d112b8da76347ddb875e31bd3b
Andrew Heins <andrew.heins@gmail.com>
2024-04-04 12:55:43 -0400

    Update example-migration-script.js

--- example-migration-script.js
+++ example-migration-script.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+
 import sanitizeHtml from 'sanitize-html';
 import { Schema } from '@sanity/schema';
 import { htmlToBlocks, getBlockContentFeatures } from '@sanity/block-tools';
@@ -24,6 +24,45 @@ const defaultSchema = Schema.compile({
           of: [{ type: 'block' }],
         },
       ],
-    }
-    ]
+    },
+  ],
+});
+
+// The compiled schema type for the content type that holds the block array
+const blockContentType = defaultSchema.get('blogPost').fields.find((field) => field.name === 'body').type;
+
+const filePath = './pages.json';
+
+// Read the JSON file
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+
+  // Parse the JSON data
+  const json = JSON.parse(data);
+
+  const pages = json.rss.channel.item;
+
+  // Log the entries
+  // console.log(pages);
+
+  // Example: Accessing entries
+  pages.forEach((page) => {
+    const pageContent = page.encoded[0].__cdata;
+    const clean = sanitizeHtml(pageContent, {
+      nonBooleanAttributes: ['*'],
+    });
+
+    const cleanedContent = clean.replace(/<p>\\s*<\\/p>|<div>\\s*<\\/div>|<section>\\s*<\\/section>|<article>\\s*<\\/article>/g, '').replace(/^\\s*\\n/gm, '');
+
+    const document = {
+      title: page.title,
+      content: htmlToBlocks(cleanedContent, blockContentType, {
+        parseHtml: (html) => new JSDOM(html).window.document,
+      }),
+    };
+    console.log(document);
+  });
 });
